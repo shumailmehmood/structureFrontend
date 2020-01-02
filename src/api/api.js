5aa2a73b8f46f2acac96a3e89f7b5db1a2b66f05
Ibrahim <34368826+ibrahimazam11@users.noreply.github.com>
2020-01-02 15:02:36 +0500

    .env and url setup

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 //help https://stackoverflow.com/questions/49500379/typical-file-structure-in-reactjs-application-grouping-api-calls-in-api-js
 import axios from 'axios';
 import resolve from './resolve';
+require('dotenv').config()
 
-let apiBaseUrl = 'http://localhost:3001';
+let apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
 
 export const testAuth = async () => {
